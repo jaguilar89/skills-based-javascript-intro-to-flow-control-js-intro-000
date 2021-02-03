af91b89bfe8e7e1be96e3f1ed8a5642ee593c1ec
funny-queue-cb37 <jaguilarny@gmail.com>
2021-02-03 23:45:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,6 +29,12 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 switch(age) {
   case age = 13:
+  case age = 14:
+  case age = 15:
+  case age = 16:
+  case age = 17:
+  case age = 18:
+  case age = 19:
     return "You are a teenager"
     break;
   default:
